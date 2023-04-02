0ed1fc2e0bd271dabd5b2f71c1211047e9bcc5a3
ssssssswh <129297286+ssssssswh@users.noreply.github.com>
2023-04-02 21:45:07 +0800

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,1 +1,19 @@
-# -
+#pragma once
+typedef int Rank; //秩
+#define DEFAULT_CAPACITY  3 //默认的初始容量（实际应用中可设置为更大）​
+template <typename T> class Vector { //向量模板类
+protected:
+	void Shuffle(std::vector<Complex>& vec);
+	int Find(const std::vector<Complex>& vec, const Complex& c);
+	void Insert(std::vector<Complex>& vec, const Complex& c);
+	bool Delete(std::vector<Complex>& vec, const Complex& c);
+	void Unique(std::vector<Complex>& vec);
+	void BubbleSort(std::vector<Complex>& vec);
+	void MergeSort(std::vector<Complex>& vec, int start, int end);
+		int mid = start + (end - start) / 2;
+	std::vector<Complex> tmp(end - start + 1);
+	int i = start, j = mid + 1, k = 0;
+	bool IsSorted(const std::vector<Complex>& vec);
+	void TestSort(std::vector<Complex>& vec);
+		std::vector<Complex> vec_bubble(vec);
+}
